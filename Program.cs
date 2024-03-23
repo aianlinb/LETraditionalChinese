a3a365813ffbbc721beb361101f08dc0c4b02c10
aianlinb <ianlin0110g@gmail.com>
2024-03-23 20:59:37 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System.IO.Compression;
+#if DEBUG
+using System.Runtime.ExceptionServices;
+#endif
+using System.Security.Cryptography;
+
+namespace LETraditionalChinese;
+
+public static class Program {
+	public static void Main(string[] args) {
+		/*var hash = GenerateHashAsync(@"E:\Steam\steamapps\common\Last Epoch\Last Epoch_Data\StreamingAssets\aa\StandaloneWindows64\localization-string-tables-chinese(simplified)(zh)_assets_all.bundle")
+			.AsTask().GetAwaiter().GetResult();
+		File.WriteAllBytes("bundle.md5", hash);
+		return;*/
+
+		var zhZip = AppContext.BaseDirectory + "zh-Hant.zip";
+		var fontZip = AppContext.BaseDirectory + "jf-openhuninn-2.0.zip";
+		if (!File.Exists(zhZip)) {
+			Console.WriteLine("Missing file: zh-Hant.zip");
+			goto end;
+		}
+		if (!File.Exists(fontZip)) {
+			Console.WriteLine("Missing file: jf-openhuninn-2.0.zip");
+			goto end;
+		}
+
+		string path;
+		if (args.Length == 0) {
+			path = SteamPath.FindGamePath("899770")!; // Last Epoch
+			if (path is null) {
+				Console.WriteLine("Unable to find the game path");
+				Console.WriteLine("Usage: LETraditionalChinese <gameInstallationPath>");
+				goto end;
+			}
+		} else if (args.Length != 1) {
+			Console.WriteLine("Usage: LETraditionalChinese [gameInstallationPath]");
+			goto end;
+		} else
+			path = args[0];
+		Console.WriteLine("Found game path: " + path);
+
+		try {
+			Console.WriteLine("Patching localization . . .");
+			var bundlePath = path + @"/Last Epoch_Data/StreamingAssets/aa/StandaloneLinux64";
+			if (!Directory.Exists(bundlePath))
+				bundlePath = path + @"/Last Epoch_Data/StreamingAssets/aa/StandaloneWindows64/localization-string-tables-chinese(simplified)(zh)_assets_all.bundle";
+
+			var info = new FileInfo(bundlePath);
+			if (info.Length > 819200) { // > 800 KB (Haven't patched yet)
+				using var za = ZipFile.OpenRead(zhZip);
+				if (za.Entries.FirstOrDefault(e => e.Name == "bundle.md5") is ZipArchiveEntry md5) { // Allow to skip the check by deleting the file
+					Span<byte> expected = stackalloc byte[16];
+					using (var s = md5.Open())
+						s.ReadExactly(expected);
+					using var fs = File.OpenRead(bundlePath);
+					if (!expected.SequenceEqual(MD5.HashDataAsync(fs).AsTask().GetAwaiter().GetResult())) {
+						Console.WriteLine();
+						Console.WriteLine("The localization has been updated by official. Please update this program.");
+						Console.WriteLine("官方已更新簡中翻譯，請更新繁體語系後再套用");
+						goto end;
+					}
+				}
+			}
+			
+			LELocalePatch.Program.Run(bundlePath, "zh-Hant.zip", false);
+			Console.WriteLine("Patching fonts . . .");
+			LEFontPatch.Program.Run(path + @"/Last Epoch_Data", "jf-openhuninn-2.0.zip");
+			return; // Do not pause if success
+		} catch (Exception ex) {
+			var tmp = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Error");
+			Console.Error.WriteLine(ex);
+			Console.ForegroundColor = tmp;
+#if DEBUG
+			ExceptionDispatchInfo.Capture(ex).Throw(); // Throw	to the debugger
+#endif
+		}
+	end:
+		Console.WriteLine();
+		Console.Write("Enter to exit . . .");
+		Console.ReadLine();
+	}
+
+	public static async ValueTask<byte[]> GenerateHashAsync(string bundlePath, CancellationToken cancellationToken = default) {
+		using var fs = File.OpenRead(bundlePath);
+		return await MD5.HashDataAsync(fs, cancellationToken);
+	}
+}
---
+++ SteamPath.cs
@@ -0,0 +1,104 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+using Microsoft.Win32;
+
+namespace LETraditionalChinese;
+public static partial class SteamPath {
+#if WINDOWS
+	public static string? GetSteamPath() {
+		string? result;
+		try {
+#pragma warning disable CA1416
+			result = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Valve\Steam", RegistryKeyPermissionCheck.ReadSubTree, System.Security.AccessControl.RegistryRights.QueryValues)?.GetValue("InstallPath", null) as string;
+			if (Directory.Exists(result))
+				return result;
+
+			result = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Valve\Steam", RegistryKeyPermissionCheck.ReadSubTree, System.Security.AccessControl.RegistryRights.QueryValues)?.GetValue("InstallPath", null) as string;
+#pragma warning restore CA1416
+			if (Directory.Exists(result))
+				return result;
+		} catch { /*Ignore*/ }
+
+		result = nint.Size == sizeof(int) ? @"C:\Program Files\Steam" : @"C:\Program Files (x86)\Steam";
+		if (Directory.Exists(result))
+			return result;
+#else // !WINDOWS
+		var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile, Environment.SpecialFolderOption.DoNotVerify);
+#if MACOS
+		var result = home + "/Library/Application Support/Steam";
+		if (Directory.Exists(result))
+			return result;
+#else // !MACOS
+		var result = home + "/.steam/steam";
+		if (Directory.Exists(result))
+			return result;
+
+		result = home + "/.local/share/Steam";
+		if (Directory.Exists(result))
+			return result;
+#endif // MACOS
+#endif // WINDOWS
+		return null;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string? GetSteamAppsPath() {
+		var steam = GetSteamPath();
+		return steam is null ? null : steam + (Path.DirectorySeparatorChar +
+#if WINDOWS
+			"steamapps"
+#else
+			"SteamApps"
+#endif
+		);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string? GetSteamAppsPath(string? steamPath) {
+		return steamPath is null ? null : steamPath + (Path.DirectorySeparatorChar +
+#if WINDOWS
+			"steamapps"
+#else
+			"SteamApps"
+#endif
+		);
+	}
+
+	[GeneratedRegex("\"path\"\\s*\"([^\"]+)\"")]
+	private static partial Regex LibraryRegex();
+	public static IEnumerable<string> GetSteamLibraryFoldersPath() {
+		var steamapps = GetSteamAppsPath();
+		if (steamapps is null)
+			yield break;
+
+		if (Directory.Exists(steamapps))
+			yield return steamapps;
+
+		var vdf = steamapps + "/libraryfolders.vdf";
+		if (!File.Exists(vdf))
+			yield break;
+		foreach (var match in LibraryRegex().Matches(File.ReadAllText(vdf)) as IList<Match>) {
+			var path = GetSteamAppsPath(Regex.Unescape(match.Groups[1].Value));
+			if (path != steamapps && Directory.Exists(path))
+				yield return path;
+		}
+	}
+
+	[GeneratedRegex("\"installdir\"\\s*\"([^\"]+)\"")]
+	private static partial Regex GameInstallDirRegex();
+	public static string? FindGamePath(string appId) {
+		foreach (var path in GetSteamLibraryFoldersPath()) {
+			var path2 = $"{path}/appmanifest_{appId}.acf";
+			if (File.Exists(path2)) {
+				var match = GameInstallDirRegex().Match(File.ReadAllText(path2));
+				if (match.Success) {
+					path2 = $"{path}{Path.DirectorySeparatorChar}common{Path.DirectorySeparatorChar}{Regex.Unescape(match.Groups[1].Value)}";
+					if (Directory.Exists(path2))
+						return path2;
+				}
+			}
+		}
+		return null;
+	}
+}
